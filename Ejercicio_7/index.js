b15ed25fa2323fb21e87ad3f6e8e3b0f5b7311df
Yolanda <yeckdemies@gmail.com>
2024-04-07 13:59:48 +0200

    EJERCICIO 7

--- Ejercicio_7/index.js
+++ Ejercicio_7/index.js
@@ -1,1 +1,14 @@
-//const places = ["Gondor", "Mordor", "Rivendel", "La Comarca", "Nümenor"];
+//Utiliza el array para crear dinamicamente una lista ul > li de elementos en el div de html con el atributo data-function="printHere".
+const places = ['Gondor', 'Mordor', 'Rivendel', 'La Comarca', 'Nümenor'];
+
+const content = document.querySelector('[data-function="printHere"]');
+const ul = document.createElement('ul');
+
+places.forEach((element) => {
+  const li = document.createElement('li');
+  li.innerText = element;
+
+  ul.append(li);
+});
+
+content.append(ul);
