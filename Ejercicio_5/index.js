446f0217da33e16cbdcab20b5bea18da04f5cf4b
Yolanda <yeckdemies@gmail.com>
2024-04-07 13:52:02 +0200

    EJERCICIO 5

--- Ejercicio_5/index.js
+++ Ejercicio_5/index.js
@@ -1,11 +1,18 @@
-/* Basandote en el array siguiente, crea una lista ul > li dinámicamente en el html 
-que imprima cada uno de los albums.
-
+//Basandote en el array siguiente, crea una lista ul > li dinámicamente en el html que imprima cada uno de los albums.
 const albums = [
-  "De Mysteriis Dom Sathanas",
-  "Reign of Blood",
-  "Ride the Lightning",
-  "Painkiller",
-  "Iron Fist",
+  'De Mysteriis Dom Sathanas',
+  'Reign of Blood',
+  'Ride the Lightning',
+  'Painkiller',
+  'Iron Fist'
 ];
-*/
+
+const ul = document.createElement('ul');
+
+albums.forEach((element) => {
+  const li = document.createElement('li');
+  li.innerText = element;
+  ul.appendChild(li);
+});
+
+document.body.append(ul);
